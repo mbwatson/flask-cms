52199d3e3dee94cbb910ab7187b8ad96db68e762
mwatson <mattwat@gmail.com>
2018-06-15 23:53:33 -0400

    remove menu interactivity

--- static/js/nav.js
+++
@@ -1,8 +0,0 @@
-const navToggler = document.querySelector('.nav-toggler')
-const navBar = document.querySelector('.navBar')
-
-navBar.addEventListener('click', () => {
-    document.body.classList.toggle('nav-out')
-})
-
-alert('asdasdasd');
